4b329771f176de3b36d51be0bc14e623a513fcc5
yousafmufc <yousafmufc@gmail.com>
2020-01-03 16:08:33 -0500

    Completed Class 7 gist project

--- gist_search/search.py
+++ gist_search/search.py
@@ -5,3 +5,33 @@ def search_gists(username, description=None, file_name=None):
     if not description and not file_name:
         print("At least one search parameter must be specified")
         return
+      
+    
+    results = []
+    
+    gists = get_gists(username)
+    
+    if gists is None:
+      print("User {} has no gists to share".format(username))
+      
+      return  # By default this will return None
+    
+    for gist in gists:
+      
+      if description and description.lower() not in gist['description'].lower():
+        continue
+        
+      
+      if file_name:
+        matched = False #Defining this just so we don't have to append gist inside the for loop and we can use break/continue.
+        for fname, fbody in gist['files'].items():
+          if file_name in fname:
+            matches = True
+            break
+          if not matched:
+            continue
+      results.append(gist)
+            
+    
+    
+    return results
--- gist_search/utils.py
+++ gist_search/utils.py
@@ -9,3 +9,17 @@ def get_gists(username):
     if not resp.ok:
         return None
     return resp.json()
+  
+if __name__ == '__main__': # this will cause below code to run only when we are explicitly focused on this current module utils.py
+  
+  gists = get_gists('gvanrossum')
+
+  first_three_gists = gists[:3]
+
+  for gist in first_three_gists:
+
+    print('{} - {}'.format(gist['id'], gist["description"]))
+    
+  
+  
+  
